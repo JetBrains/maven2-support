dc1092f527942e1ce4fcf99e0ad30d6c1804f796
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2022-08-08 17:41:54 +0200

    plugin.xml;

---
+++ src/main/java/org/jetbrains/idea/maven2/maven2support/Maven2SupportExtension.java
@@ -0,0 +1,82 @@
+package org.jetbrains.idea.maven2.maven2support;
+
+import com.intellij.openapi.application.PathManager;
+import com.intellij.openapi.roots.ui.distribution.DistributionInfo;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.util.PathUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.idea.maven.MavenVersionAwareSupportExtension;
+import org.jetbrains.idea.maven.model.MavenId;
+import org.jetbrains.idea.maven.project.MavenProjectBundle;
+import org.jetbrains.idea.maven.server.MavenDistribution;
+import org.jetbrains.idea.maven.server.MavenDistributionsCache;
+import org.jetbrains.idea.maven.server.MavenServer;
+import org.jetbrains.idea.maven.utils.MavenUtil;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static org.jetbrains.idea.maven.server.MavenServerManager.BUNDLED_MAVEN_2;
+import static org.jetbrains.idea.maven.server.MavenServerManager.BUNDLED_MAVEN_3;
+
+public class Maven2SupportExtension implements MavenVersionAwareSupportExtension {
+    public static final String BUNDLED_MAVEN_2 = "Bundled (Maven 2)";
+
+    @Override
+    public boolean isSupportedByExtension(@Nullable File mavenHome) {
+        String version = MavenUtil.getMavenVersion(mavenHome);
+        return StringUtil.compareVersionNumbers(version, "3") < 0;
+    }
+
+    @Override
+    public @Nullable File getMavenHomeFile(@Nullable String mavenHomeName) {
+        return null;
+    }
+
+    @Override
+    public @Nullable String asMavenHome(DistributionInfo distribution) {
+        return null;
+    }
+
+    @Override
+    public @Nullable DistributionInfo asDistributionInfo(String mavenHome) {
+        return null;
+    }
+
+    @Override
+    public @NotNull List<File> collectClassPathAndLibsFolder(@NotNull MavenDistribution distribution) {
+        String pluginsRoot = PathManager.getPluginsPath();
+        final List<File> classpath = new ArrayList<>();
+
+        classpath.add(new File(PathUtil.getJarPathForClass(MavenId.class)));
+        classpath.add(new File(PathUtil.getJarPathForClass(MavenServer.class)));
+
+        addDir(classpath, new File(pluginsRoot, "maven2-support/server"), file -> file.getName().endsWith(".jar"));
+        addMavenLibs(classpath, distribution.getMavenHome().toFile());
+        return classpath;
+    }
+
+    private static void addDir(List<File> classpath, File dir, Predicate<File> filter) {
+        File[] files = dir.listFiles();
+        if (files == null) return;
+
+        for (File jar : files) {
+            if (jar.isFile() && jar.getName().endsWith(".jar") && filter.test(jar)) {
+                classpath.add(jar);
+            }
+        }
+    }
+
+    private static void addMavenLibs(List<File> classpath, File mavenHome) {
+        addDir(classpath, new File(mavenHome, "lib"), f -> !f.getName().contains("maven-slf4j-provider"));
+        File bootFolder = new File(mavenHome, "boot");
+        File[] classworldsJars = bootFolder.listFiles((dir, name) -> StringUtil.contains(name, "classworlds"));
+        if (classworldsJars != null) {
+            Collections.addAll(classpath, classworldsJars);
+        }
+    }
+}
