665964a89fbb4ddb4cb0a1eecfc5608392764cf8
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2022-12-22 19:10:43 +0100

    update version to 1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "org.jetbrains.idea.maven"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 val ideaVersion by rootProject.extra { "231-SNAPSHOT" }
 val ideaType by rootProject.extra { "IC" }
